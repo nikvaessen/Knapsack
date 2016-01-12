73ac5ca95a34ffdad452fe0e23e7ca7d9778f282
bianca26 <biby_iancu26@yahoo.com>
2016-01-12 14:45:23 +0100

    ga

--- src/HollowSpace.java
+++ src/HollowSpace.java
@@ -71,9 +71,9 @@ public class HollowSpace extends Space {
         try
         {
             int[] coords = getViableCoordinates(object);
-            System.out.println("length of object " + object.getLength() + " width of object " +
-                    object.getWidth() + " height of object " + object.getHeight()  + " placing object at:"
-             + Arrays.toString(coords));
+           // System.out.println("length of object " + object.getLength() + " width of object " +
+            //        object.getWidth() + " height of object " + object.getHeight()  + " placing object at:"
+           //  + Arrays.toString(coords));
             fill(object, value, coords[0], coords[1], coords[2]);
         }
         catch (NoRoomException e)
@@ -204,7 +204,7 @@ public class HollowSpace extends Space {
                         int value = space[x][z][y];
                         if(value == -1 && canFit(object,x,z,y))
                         {
-                            System.out.printf("returned %s as viable coordinates\n", Arrays.toString(new int[] {x,z,y}));
+                            //System.out.printf("returned %s as viable coordinates\n", Arrays.toString(new int[] {x,z,y}));
                             return new int[] {x,z,y};
                         }
                     }
