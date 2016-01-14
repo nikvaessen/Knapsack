2a48e2d3119b6c7bd632d98d74d0ecac91db97df
bianca26 <biby_iancu26@yahoo.com>
2016-01-14 13:24:53 +0100

    ga

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -34,9 +34,9 @@ public class Knapsack {
 
         //create a truck as described in the project booklet and fill it with the made set
         Truck truck = new Truck(165, 25, 40);
-        greedyFill(truck, set);
+        //greedyFill(truck, set);
         //backTrackFill(truck, set);
-        //testShit();
+        testShit();
     }
 
     public static void testShit() throws HollowSpace.NoRoomException
